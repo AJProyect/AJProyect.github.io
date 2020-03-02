4313d4c254f86ea0af2fd54ceacec232f4fb2d24
AJProyect <48093438+AJProyect@users.noreply.github.com>
2020-03-02 08:18:34 +0000

    Upload React Project

---
+++ precache-manifest.fd7a7e212baa5c230563c73ae95cc492.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a0685fa1fc2698834fc5b117ad3f1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc76c8cb3fd4530528e7",
+    "url": "/static/js/2.d2cc6f57.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.d2cc6f57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c8f12274ea059a4cf58",
+    "url": "/static/js/main.75eaec38.chunk.js"
+  },
+  {
+    "revision": "9c7f4221c05fe1ab1dc7",
+    "url": "/static/js/runtime-main.bb9e7107.js"
+  }
+]);
